2894fd370c923ce1bab8e6efb923cec968d1f406
Dipta Justin Gomes <diptagomes@hotmail.com>
2018-10-30 22:09:08 +0600

    Commit by including the dataset Nursery

--- apriori.py
+++ apriori.py
@@ -6,14 +6,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Data Preprocessing
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+dataset = pd.read_csv('Nursery.csv', header = None)
 transactions = []
+
+
 for i in range(0, 7501):
-    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 9)])
 
 # Training Apriori on the dataset
 from apyori import apriori
-rules = apriori(transactions, min_support = 0.01, min_confidence = 0.2, min_lift = 3, min_length = 2)
+rules = apriori(transactions, min_support = 0.01, min_confidence = 0.95, min_lift = 3, min_length = 2)
 
 # Visualising the results
 results = list(rules)
