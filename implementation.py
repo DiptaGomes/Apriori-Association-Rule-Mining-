e2d6faf5459938bbadba6eefa48493aa8ea3385f
Dipta Justin Gomes <diptagomes@hotmail.com>
2018-10-30 22:55:09 +0600

    New_Commit

--- implementation.py
+++ implementation.py
@@ -10,12 +10,12 @@ dataset = pd.read_csv('Nursery.csv', header = None)
 transactions = []
 
 
-for i in range(0, 7501):
+for i in range(0, 9410):
     transactions.append([str(dataset.values[i,j]) for j in range(0, 9)])
 
 # Training Apriori on the dataset
 from apyori import apriori
-rules = apriori(transactions, min_support = 0.01, min_confidence = 0.95, min_lift = 3, min_length = 2)
+rules = apriori(transactions, min_support = 0.04, min_confidence = .99, min_lift = 3, min_length = 2)
 
 # Visualising the results
 results = list(rules)
